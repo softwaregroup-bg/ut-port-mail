89d7f50e49863fd616a4354b897029f0acf336c1
Simeon Petkov <simeon.petkov@softwaregroup-bg.com>
2017-07-28 11:31:41 +0300

    fix error.js

--- errors.js
+++ errors.js
@@ -3,12 +3,13 @@ var create = require('ut-error').define;
 const PortMail = create('PortMail', undefined, 'Mail sending error');
 const Validation = create('Validation', PortMail, 'Mail validation error');
 const PortMailClient = create('PortMailClient', PortMail);
+const UnknownError = create('unknownError', PortMailClient, 'Unknown error');
 
 module.exports = {
     mail: cause => new PortMail(cause),
     validation: cause => new Validation(cause),
     badConstructorClientParams: create('badConstructorClientParams', PortMailClient, 'Bad client mail constructor params'),
     invalidCredentials: create('invalidCredentials', PortMailClient, 'Invalid credentials'),
-    unknownError: (cause) => create('unknownError', PortMailClient, cause || 'Unknown error'),
+    unknownError: cause => new UnknownError(cause),
     badMailOptionsParams: create('badMailOptionsParams', PortMailClient, 'Bad mail options params')
 };
