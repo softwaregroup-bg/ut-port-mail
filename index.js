ab75db0dcab82e4d80440b3ae0b03dc2aa3e6b24
Kristiyan Todorov <kristiyan.todorov@softwaregroup-bg.com>
2018-09-28 08:57:50 +0300

    fix: ut-error deprecations (#9)

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = function({parent}) {
             type: 'mail',
             logLevel: 'info'
         }, config);
-        errors = errors || require('./errors')(this.defineError);
+        errors = errors || require('./errors')(this.bus.errors);
     }
 
     if (parent) {
