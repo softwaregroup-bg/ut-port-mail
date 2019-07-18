6f9d1f8a63d50daa1b255fb2722f048622a9c61e
stambata <stamen.peev@softwaregroup-bg.com>
2019-07-18 08:11:52 +0300

    fix: additional mail client options

--- index.js
+++ index.js
@@ -1,46 +1,72 @@
 const MailClient = require('./client');
-
-module.exports = ({utPort, utError}) => class MailPort extends utPort {
-    get defaults() {
-        return {
-            type: 'mail'
-        };
-    }
-    start() {
-        super.start(...arguments);
-        this.pull(this.exec);
-    }
-    exec({
-        service = this.config.service,
-        host = this.config.host,
-        url = this.config.url,
-        port = this.config.port,
-        secure = this.config.secure,
-        username = this.config.username,
-        password = this.config.password,
-        from, to, subject, text, html, body, cc, bcc, replyTo, headers, attachments
-    }) {
-        return new MailClient({
-            service,
-            host,
-            url,
-            port,
-            secure,
-            auth: {
-                user: username,
-                pass: password
-            }
-        }, require('./errors')(utError)).send({
-            from,
-            to,
-            subject,
-            text,
-            html: html || body || text,
-            cc,
-            bcc,
-            replyTo,
-            headers,
-            attachments
-        });
-    }
+module.exports = ({utPort, utError}) => {
+    const additionalMailCLientOptions = {};
+    return class MailPort extends utPort {
+        get defaults() {
+            return {
+                type: 'mail'
+            };
+        }
+        async start(...params) {
+            const result = await super.start(...params);
+            [
+                'ignoreTLS',
+                'requireTLS',
+                'disableFileAccess',
+                'disableUrlAccess',
+                'tls',
+                'authMethod',
+                'name',
+                'localAddress',
+                'connectionTimeout',
+                'greetingTimeout',
+                'socketTimeout',
+                'pool',
+                'maxConnections',
+                'maxMessages',
+                'rateDelta',
+                'rateLimit',
+                'proxy'
+            ].forEach(option => {
+                const value = this.config[option];
+                if (typeof value !== 'undefined') additionalMailCLientOptions[option] = value;
+            });
+            this.pull(this.exec);
+            return result;
+        }
+        exec({
+            service = this.config.service,
+            host = this.config.host,
+            url = this.config.url,
+            port = this.config.port,
+            secure = this.config.secure,
+            username = this.config.username,
+            password = this.config.password,
+            from, to, subject, text, html, body, cc, bcc, replyTo, headers, attachments
+        }) {
+            return new MailClient({
+                service,
+                host,
+                url,
+                port,
+                secure,
+                auth: {
+                    user: username,
+                    pass: password
+                },
+                ...additionalMailCLientOptions
+            }, require('./errors')(utError)).send({
+                from,
+                to,
+                subject,
+                text,
+                html: html || body || text,
+                cc,
+                bcc,
+                replyTo,
+                headers,
+                attachments
+            });
+        }
+    };
 };
